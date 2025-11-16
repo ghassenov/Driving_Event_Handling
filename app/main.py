1ad991f36253136a105f09be440081fe58de500e
ghassen naouar <ghassennaouar7@gmail.com>
2025-11-16 17:40:37 +0100

    feat: add CORS middleware and include routers

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from database.db import Base, engine
+from routes.auth_routes import router as auth_router
+from routes.predict_routes import router as predict_router
+from routes.trip_routes import router as trip_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+app.include_router(predict_router, prefix="/predict", tags=["Prediction"])
+app.include_router(trip_router, prefix="/trips", tags=["Trips"])
