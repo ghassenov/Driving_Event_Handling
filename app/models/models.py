a8433ed48fbada11dbc082515b6e88e796f0b32d
ghassen naouar <ghassennaouar7@gmail.com>
2025-11-16 17:33:22 +0100

    feat: add User and trip tables

---
+++ app/models/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from database.db import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+
+    trips = relationship("Trip", back_populates="user")
+
+
+class Trip(Base):
+    __tablename__ = "trips"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    score = Column(Float)
+
+    user = relationship("User", back_populates="trips")
