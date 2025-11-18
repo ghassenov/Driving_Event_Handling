81b2ac620384f21e84fa0edbdd43669044067ff8
ghassen naouar <ghassennaouar7@gmail.com>
2025-11-18 21:17:12 +0100

    feat: add user router

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from database.db import Base, engine
 from routes.auth_routes import router as auth_router
 from routes.predict_routes import router as predict_router
 from routes.trip_routes import router as trip_router
+from routes.car_routes import router as car_router
+from routes.user_routes import router as user_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -16,6 +18,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(user_router, prefix="/users", tags=["Users"])
+app.include_router(car_router, prefix="/cars", tags=["Cars"])
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(predict_router, prefix="/predict", tags=["Prediction"])
 app.include_router(trip_router, prefix="/trips", tags=["Trips"])
